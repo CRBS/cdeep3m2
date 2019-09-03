1b15217145aebd4f7423fe8bbf7360980460b233
spenticoff <spenticoff@ucsd.edu>
2019-09-03 10:12:28 -0700

    Update dim_convert.py

--- dim_convert.py
+++ dim_convert.py
@@ -10,18 +10,18 @@ import numpy as np
 
 def dim_convert(img, lb):
     shape = img.shape
-    print (shape)
+    print(shape)
     temp_img = np.zeros([shape[1], shape[2], shape[0]], dtype=img.dtype)
     temp_lb = np.zeros([shape[1], shape[2], shape[0]], dtype=lb.dtype)
-    #temp_img = np.zeros([1024, 1024, shape[0]], dtype=img.dtype)
-    #temp_lb = np.zeros([1024,1024, shape[0]], dtype=lb.dtype)
+    # temp_img = np.zeros([1024, 1024, shape[0]], dtype=img.dtype)
+    # temp_lb = np.zeros([1024,1024, shape[0]], dtype=lb.dtype)
     for z in range(0, shape[0]):
         temp_img[0:shape[1], 0:shape[2], z] = np.squeeze(img[z, :, :])
         temp_lb[0:shape[1], 0:shape[2], z] = np.squeeze(lb[z, :, :])
-    #print (temp_img.shape)
-    #print (temp_lb.shape)
+    # print(temp_img.shape)
+    # print(temp_lb.shape)
     img = np.squeeze(temp_img)
     lb = np.squeeze(temp_lb)
-    #print (img.shape)
-    #print (lb.shape)
+    # print(img.shape)
+    # print(lb.shape)
     return img, lb
