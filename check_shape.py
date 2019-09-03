9316ed9fe6cc9985016bc10703588a61ec92d671
spenticoff <spenticoff@ucsd.edu>
2019-09-03 10:19:27 -0700

    Update check_shape.py

--- check_shape.py
+++ check_shape.py
@@ -9,4 +9,4 @@ for filename in os.listdir(
             '/scratch/converted_all_python/test_testsample_processed/converted/' +
             filename,
             'r')
-        print (f['data'].shape)
+        print(f['data'].shape)
