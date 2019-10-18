2d4e56305f34d39ae0f064b18b70b052c2189e78
mhaberl <mhaberl@ucsd.edu>
2019-10-18 14:50:03 -0700

    added read_files_in_folder.py

---
+++ functions/read_files_in_folder.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import stat
+
+def read_files_in_folder(input_directory):
+    """
+Read_files_in_folder
+   Get the complete list of good excluding hidden Files, excluding any subfolders in the input folder
+   
+
+   INPUT FORMAT
+   --------------------------
+   (InputDirectory)
+
+   OUTPUT FORMAT
+   --------------------------
+   [fileList, file_list_length]
+
+   
+   --------------------------
+   -- National Center for Microscopy and Imaging Research, NCMIR
+   -- Matthias Haberl -- San Diego, 02/2016"""
+
+    fileList = [file_name for file_name in os.listdir(input_directory) 
+                if not os.path.isdir(file_name) and not file_name.startswith('.')]
+
+    if(sys.platform.startswith("win")):
+        fileList = [file_name for file_name in fileList 
+                    if not bool(os.stat(os.path.join(input_directory, file_name)).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)]
+
+    return [sorted(fileList), len(fileList)]
