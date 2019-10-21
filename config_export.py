100ef66747ced0ca0453417d423413d70002cfe0
spenticoff <spenticoff@ucsd.edu>
2019-10-21 08:18:22 -0700

    Update config_export.py

--- config_export.py
+++ config_export.py
@@ -13,25 +13,24 @@ Created on Wed Sep 18 23:08:50 2019
 """
 import os.path
 def writecfg(outdir, version, addtl_choices, strength, thirdstrength):
-            
 
-    completeName = os.path.abspath(outdir + "/Config Settings v" + str(version) +  ".txt")         
+    completeName = os.path.abspath(outdir + "/Config Settings v" + str(version) +  ".txt")
     file = open(completeName, "w")
     file.write('secondary augmentation strength:')
     file.write(strength +  '\n')
     file.write('tertiary augmentation strength:')
     file.write(thirdstrength + '\n')
     file.write('0: Original Image \n')
-    for i in range(1,8):
+    for i in range(1, 8):
         file.write(str(i))
         file.write(': ')
-        file.write(str(addtl_choices[i]).split(" ",2)[1])
+        file.write(str(addtl_choices[i]).split(" ", 2)[1])
         file.write('\n')
-    for i in range(9,16):
+    for i in range(9, 16):
         file.write(str(i))
         file.write(': ')
-        file.write(str(addtl_choices[i][0]).split(" ",2)[1])
+        file.write(str(addtl_choices[i][0]).split(" ", 2)[1])
         file.write(', ')
-        file.write(str(addtl_choices[i][1]).split(" ",2)[1])
+        file.write(str(addtl_choices[i][1]).split(" ", 2)[1])
         file.write('\n')
     file.close()
