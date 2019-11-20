f119b482740430a7729b5fe921bcba00cbada185
mhaberl <mhaberl@ucsd.edu>
2019-11-20 14:26:40 -0800

    Update add_z_padding.py

--- add_z_padding.py
+++ add_z_padding.py
@@ -1,8 +1,8 @@
 """
-Adds 2 planes in the begin and end of the image stack
+Adds 2 planes for z padding in the begin and end of the image stack
 
-NCMIR/NBCR, UCSD -- CDeep3M -- Last update: 08/2019
-
+NCMIR, UCSD -- CDeep3M -- Last update: 11/2019
+@MHaberl
 """
 
 import numpy as np
@@ -11,11 +11,11 @@ import numpy as np
 def add_z_padding(im_stack):
     print('Adding Z padding')
     print(np.shape(im_stack))
-    im_stack = np.concatenate((im_stack[2:3],
-                               im_stack[1:2],
+    im_stack = np.concatenate((im_stack[:1],
+                               im_stack[:1],
                                im_stack,
-                               im_stack[-2:-1],
-                               im_stack[-3:-2]),
+                               im_stack[-1:],
+                               im_stack[-1:]),
                               axis=0)
     print(np.shape(im_stack))
     return im_stack
