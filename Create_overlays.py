8fb9627c7cf98143e97ed865947ae5f14c90544f
mhaberl <mhaberl@ucsd.edu>
2020-03-01 17:43:10 -0800

    Create_overlays.py, faster and reduce memory requ

--- Create_overlays.py
+++ Create_overlays.py
@@ -43,20 +43,26 @@ def processInput(x):
     raw_image = cv2.imread(file_in, -1)
     file_in = os.path.join(inputfolder_seg, file_list_seg[x])
     # sys.stdout.write('Loading: ' + str(file_in) + ' -> ')
-    seg = cv2.imread(file_in, -1)    
+    seg = cv2.imread(file_in, -1)
     seg = np.uint8(seg)
-    seg = np.clip(seg, 40, 255)
+    #seg = np.clip(seg, 40, 255)
     seg = skimage.exposure.rescale_intensity(seg, in_range=(40, 215), out_range=(0, 255))
-    rgb_seg = np.dstack((np.zeros_like(seg), seg, np.zeros_like(seg))) #convert to RGB for overlay
+    #rgb_seg = np.dstack((np.zeros_like(seg), seg, np.zeros_like(seg))) #convert to RGB for overlay
     raw_image = np.uint8(raw_image)
     raw_image = skimage.exposure.rescale_intensity(raw_image, in_range=(0, 235), out_range=(0, 255))
-    rgb_img = np.dstack((raw_image, raw_image, raw_image)) #convert to RGB image for overlay
-    overlayed = rgb_img # initialize
-    overlayed = np.uint8(np.uint8(0.60* rgb_img) + np.uint8(0.40* rgb_seg))
+    #rgb_img = np.dstack((raw_image, raw_image, raw_image)) #convert to RGB image for overlay
+    #overlayed = rgb_img # initialize
+    #overlayed = np.uint8(np.uint8(0.60* rgb_img) + np.uint8(0.40* rgb_seg))
+    seg =  cv2.merge([np.zeros_like(seg), seg, np.zeros_like(seg)])
+    overlayed = cv2.addWeighted(seg, 0.4, cv2.cvtColor(raw_image, cv2.COLOR_GRAY2BGR), 0.6, 0)
+    del seg
+    del raw_image
     overlayed = skimage.exposure.rescale_intensity(overlayed, in_range=(0, 225), out_range=(0, 255))
+    #file_out = os.path.join(outputfolder, file_list_seg[x])
     file_out = os.path.join(outputfolder, 'overlay_%04d.png' % (x+1) )
     sys.stdout.write('Saving: ' + str(file_out) + '\n')
-    skimage.io.imsave(file_out, overlayed)
+    #skimage.io.imsave(file_out, overlayed)
+    cv2.imwrite(file_out, overlayed)
 
 p_tasks = max(1, min(len(file_list_raw), int(cpu_count() / 2)))
 sys.stdout.write('Running ' + str(p_tasks) + ' parallel tasks\n')
