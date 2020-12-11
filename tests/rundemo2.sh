d5970897f4e7ef76a380b4c23cccb838bb025397
mhaberl <mhaberl@ucsd.edu>
2020-12-11 08:50:55 -0800

    Update rundemo2.sh

--- tests/rundemo2.sh
+++ tests/rundemo2.sh
@@ -11,8 +11,8 @@ echo 'Running Demo2 on CDeep3M Version'
 cat $cdeep3m_folder/VERSION
 echo "Code directory: $cdeep3m_folder"
 $cdeep3m_folder/PreprocessTrainingData.py $cdeep3m_folder/mito_testsample/training/images/ $cdeep3m_folder/mito_testsample/training/labels/ $cdeep3m_folder/mito_testsample/mito_test_augmented
-$cdeep3m_folder/runtraining.sh --numiterations 50 --snapshot_interval 10 $cdeep3m_folder/mito_testsample/mito_test_augmented $cdeep3m_folder/mito_testsample/demo_trained
+$cdeep3m_folder/runtraining.sh --numiterations 80 --snapshot_interval 10 $cdeep3m_folder/mito_testsample/mito_test_augmented $cdeep3m_folder/mito_testsample/demo_trained
 $cdeep3m_folder/runprediction.sh $cdeep3m_folder/mito_testsample/demo_trained/ $cdeep3m_folder/mito_testsample/testset/ $cdeep3m_folder/mito_testsample/demo_predictout
-$cdeep3m_folder/PlotValidation.py $cdeep3m_folder/mito_testsample/demo_predictout/1fm/log/
-$cdeep3m_folder/PlotValidation.py $cdeep3m_folder/mito_testsample/demo_predictout/3fm/log/
-$cdeep3m_folder/PlotValidation.py $cdeep3m_folder/mito_testsample/demo_predictout/5fm/log/
+$cdeep3m_folder/PlotValidation.py $cdeep3m_folder/mito_testsample/demo_trained/1fm/log/
+$cdeep3m_folder/PlotValidation.py $cdeep3m_folder/mito_testsample/demo_trained/3fm/log/
+$cdeep3m_folder/PlotValidation.py $cdeep3m_folder/mito_testsample/demo_trained/5fm/log/
